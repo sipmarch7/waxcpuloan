e290429a16258c404e87830665c1759a65f80516
Spyridon Peppas <speppas@iit.demokritos.gr>
2021-11-11 17:41:33 +0200

    router and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { MyListingsComponent } from './components/my-listings/my-listings.component';
+import { SubHeaderComponent } from './components/sub-header/sub-header.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'myListings', component: MyListingsComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SubHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
